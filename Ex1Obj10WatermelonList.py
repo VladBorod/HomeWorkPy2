a0000a144ebbd78e74210318ec2bfe97500c3896
VladBorod <jazgigantsmith@gmail.com>
2023-02-01 23:28:35 +0300

    First task, LIST

--- Ex1Obj10WatermelonList.py
+++ Ex1Obj10WatermelonList.py
@@ -9,23 +9,6 @@
 # Input: 5 -> 5 1 6 5 9
 # Output: 1 9
 
-# watermelon_number = int(input('Введите количество арбузов: '))
-# watermelon_max = 0
-# watermelon_min = 0
-# watermelon_temp = 0
-
-# while watermelon_number > 0:
-#     watermelon_weight = int(input('Введите вес арбузов: '))
-#     watermelon_number -= 1
-#     watermelon_temp = watermelon_weight
-#     if watermelon_weight > watermelon_temp:
-#         watermelon_max = watermelon_weight
-#     else:
-#         watermelon_min = watermelon_weight
-
-#     print('макс', watermelon_max, 'мин', watermelon_min, 'temp', watermelon_temp)
-
-# watermelon_list = []
 watermelon_list = list(map(int, input('Введите вес арбузов через пробел: ').split()))
 watermelon_list.sort()
 print('Наименьший арбуз -',watermelon_list[0], 'Наибольший арбуз -',watermelon_list[-1])
